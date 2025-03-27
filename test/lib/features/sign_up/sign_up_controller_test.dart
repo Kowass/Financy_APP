47b6281dbe4617acf1ba3d0402c0e895fdc655a6
FELIPE de Sousa Alves B902040 <b902040@bnb.gov.br>
2025-03-27 08:25:02 -0300

    conclusão testes

--- test/lib/features/sign_up/sign_up_controller_test.dart
+++ test/lib/features/sign_up/sign_up_controller_test.dart
@@ -2,18 +2,22 @@ import 'package:financy_app/commom/models/user_model.dart';
 import 'package:financy_app/features/sign_up/sign_up_controller.dart';
 import 'package:financy_app/features/sign_up/sign_up_state.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
 import '../../mock/mock_classes.dart';
 
 void main() {
   late SignUpController signUpController;
-  late MockSecureStorage secureStorage;
-  late MockFirebaseAuthService firebaseAuthService;
+  late MockSecureStorage mockSecureStorage;
+  late MockFirebaseAuthService mockFirebaseAuthService;
   late UserModel user;
   setUp(() {
-    secureStorage = MockSecureStorage();
-    firebaseAuthService = MockFirebaseAuthService();
-    signUpController = SignUpController(firebaseAuthService, secureStorage);
+    mockSecureStorage = MockSecureStorage();
+    mockFirebaseAuthService = MockFirebaseAuthService();
+
+    signUpController =
+        SignUpController(mockFirebaseAuthService, mockSecureStorage);
+        
     user = UserModel(
       name: "User",
       email: "user@email.com",
@@ -21,13 +25,47 @@ void main() {
     );
   });
 
-  test('Test sign up controller', () async {
+  test('Test sign up controller Success State', () async {
     expect(signUpController.state, isInstanceOf<SignUpStateIntial>());
+
+    when(() => mockSecureStorage.write(
+          key: "CURRENT_USER",
+          value: user.toJson(),
+        )).thenAnswer((_) async {});
+
+    when(() => mockFirebaseAuthService.signUp(
+          name: 'User',
+          email: 'user@email.com',
+          password: 'user@123',
+        )).thenAnswer((_) async => user,);
+
     await signUpController.signUp(
       name: "User",
       email: "user@email.com",
       password: "user@123",
-    );    
+    );
     expect(signUpController.state, isInstanceOf<SignUpStateSuccess>());
+  });
+
+  test('Test sign up controller Error State', () async {
+    expect(signUpController.state, isInstanceOf<SignUpStateIntial>());
+
+    when(() => mockSecureStorage.write(
+          key: "CURRENT_USER",
+          value: user.toJson(),
+        )).thenAnswer((_) async {});
+
+    when(() => mockFirebaseAuthService.signUp(
+          name: 'User',
+          email: 'user@email.com',
+          password: 'user@123',
+        )).thenThrow(Exception());
+
+    await signUpController.signUp(
+      name: "User",
+      email: "user@email.com",
+      password: "user@123",
+    );
+    expect(signUpController.state, isInstanceOf<SignUpStateError>());
   });
 }
--- test/lib/mock/mock_classes.dart
+++ test/lib/mock/mock_classes.dart
@@ -1,4 +1,3 @@
-import 'package:financy_app/features/sign_up/sign_up_controller.dart';
 import 'package:financy_app/services/auth_service.dart';
 import 'package:financy_app/services/secure_storage.dart';
 import 'package:mocktail/mocktail.dart';
@@ -6,5 +5,3 @@ import 'package:mocktail/mocktail.dart';
 class MockFirebaseAuthService extends Mock implements AuthService {}
 
 class MockSecureStorage extends Mock implements SecureStorage {}
-
-class MockSignUpController extends Mock implements SignUpController {}
